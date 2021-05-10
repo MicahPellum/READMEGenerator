3febfc56069bf9d09d161f2fd6439190a82873bf
Micah Pellum <pellummicah@gmail.com>
2021-05-10 10:03:21 -0600

    moved files around

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# READMEGenerator
+# Professional README Generator Starter Code
+
+[How to create a Professional README](./readme-guide.md)
+
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown")
+const generateMarkdown = require("./Develop/utils/generateMarkdown")
 const util = require("util");
 const writeFileAsync = util.promisify(fs.writeFile);
 
